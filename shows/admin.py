c153095c5730575b573b5e15b0e79bc05a365119
Dan Garcia <garciadd@gmail.com>
2016-07-10 01:11:31 -0500

    update models and urls

--- shows/admin.py
+++ shows/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Show, Stage, Venue
+
 # Register your models here.
+admin.site.register(Show)
+admin.site.register(Stage)
+admin.site.register(Venue)
--- shows/views.py
+++ shows/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Show, Stage, Venue
 
 # Create your views here.
+def index(request):
+    return HttpResponse("You're at the shows index")
+
+def stage_index(request):
+    return HttpResponse("You're at the stages index")
+
+def venue_index(request):
+    return HttpResponse("You're at the venues index")    
+
+def show_detail(request, show_id):
+    return HttpResponse("You're looking at show %s." % show_id)
+
+def stage_detail(request, stage_id):
+    return HttpResponse("You're looking at stage %s." % stage_id)
+
+def venue_detail(reqeust, venue_id):
+    return HttpResponse("You're looking at venue %s.", venue_id)
